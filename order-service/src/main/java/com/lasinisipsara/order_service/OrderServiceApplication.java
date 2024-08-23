571a20f0b6bc420c73e318a2402da75af49c9c4f
LasiSipsara <sipsaralasini@gmail.com>
2024-08-23 13:43:59 +0530

    Replace Spring Cloud Open Feign with RestClient

--- order-service/src/main/java/com/lasinisipsara/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/lasinisipsara/order_service/OrderServiceApplication.java
@@ -2,10 +2,9 @@ package com.lasinisipsara.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+
 public class  OrderServiceApplication {
 
 	public static void main(String[] args) {
